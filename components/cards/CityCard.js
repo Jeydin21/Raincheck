0b37ac68079458bd7b894492dad69d65b1be382f
Jeydin21 <JeyPham21@gmail.com>
2024-09-23 10:54:54 -0500

    STarted working on little weather details card inside the card

--- components/cards/CityCard.js
+++ components/cards/CityCard.js
@@ -19,29 +19,43 @@ function CityCard({ weatherData, loading }) {
         </>
       ) : (
         <>
-          <p className="text-lg md:text-4xl text-gray-300 mb-12">
-              <img 
-                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} 
-                alt={weatherData.weather[0].description} 
-                className="inline-block w-12 h-12" 
-              />
-              {((weatherData.main.temp - 273.15) * 9 / 5 + 32).toFixed(0)}°F
+          <p className="text-lg md:text-4xl text-gray-300 mb-6">
+            <img
+              src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+              alt={weatherData.weather[0].description}
+              className="inline-block w-12 h-12"
+            />
+            {((weatherData.main.temp - 273.15) * 9 / 5 + 32).toFixed(0)}°F
           </p>
-          <p className="text-lg md:text-xl text-gray-300 mb-12">
-            Description: {weatherData.weather[0].description.charAt(0).toUpperCase() + weatherData.weather[0].description.slice(1)}
+          <p className="text-lg md:text-xl text-gray-300">
+            Feels like {((weatherData.main.feels_like - 273.15) * 9 / 5 + 32).toFixed(0)}°F
           </p>
-          <p className="text-lg md:text-xl text-gray-300 mb-12">
-            Humidity: {weatherData.main.humidity}%
+          <p className="text-lg md:text-xl text-gray-300 mb-6">
+            {weatherData.weather.map((desc, index) => (
+              <span key={index}>
+                {desc.description.charAt(0).toUpperCase() + desc.description.slice(1)}
+                {index < weatherData.weather.length - 1 && ', '}
+              </span>
+            ))}
           </p>
-          <p className="text-lg md:text-xl text-gray-300 mb-12">
-            Wind Speed: {weatherData.wind.speed} m/s
-          </p>
-          <p className="text-lg md:text-xl text-gray-300 mb-12">
-            Pressure: {weatherData.main.pressure} hPa
-          </p>
-          <p className="text-lg md:text-xl text-gray-300 mb-12">
-            Visibility: {weatherData.visibility / 1000} km
-          </p>
+          <div className="flex flex-row space-x-5 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-3xl p-5 shadow-lg">
+            <div className="flex flex-col justify-center space-y-10">
+              <p className="text-lg md:text-xl text-gray-300">
+                Humidity: {weatherData.main.humidity}%
+              </p>
+              <p className="text-lg md:text-xl text-gray-300">
+                Wind Speed: {weatherData.wind.speed} m/s
+              </p>
+            </div>
+            <div className="flex flex-col justify-center space-y-10">
+              <p className="text-lg md:text-xl text-gray-300">
+                Pressure: {weatherData.main.pressure} hPa
+              </p>
+              <p className="text-lg md:text-xl text-gray-300">
+                Visibility: {weatherData.visibility / 1000} km
+              </p>
+            </div>
+          </div>
         </>
       )}
     </div>
